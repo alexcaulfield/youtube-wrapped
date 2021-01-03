1963c955c93860d4a7c7c70de8285d22643c7d0e
Alex Caulfield <alex.caulfield28@gmail.com>
2021-01-03 10:59:56 -0500

    rendering data on screen

--- src/components/data_input.tsx
+++ src/components/data_input.tsx
@@ -1,11 +1,17 @@
 import * as React from 'react';
 import {useState} from 'react';
-import TextField from '@material-ui/core/TextField';
 import {splitChannelsAndVideos, sortTopResults} from '../utils/parse_data';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
 
 const DataInput = () => {
-  const [topChannels, setTopChannels] = useState(new Map())
-  const [topVideos, setTopVideos] = useState(new Map())
+  // top 20 channels
+  const [topChannels, setTopChannels] = useState([])
+  // top 20 videos
+  const [topVideos, setTopVideos] = useState([])
 
   const loadHistoryData = event => {
     const file = event.target.files[0];
@@ -20,8 +26,10 @@ const DataInput = () => {
       const results = splitChannelsAndVideos(links);
       const sortedChannels = sortTopResults(results.channels);
       const sortedVideos = sortTopResults(results.videos);
-      console.log(sortedChannels)
-      console.log(sortedVideos)
+      const channelArray = Array.from(sortedChannels).slice(0, 20);
+      const videoArray = Array.from(sortedVideos).slice(0, 20);
+      setTopChannels(channelArray)
+      setTopVideos(videoArray)
     }
   }
   
@@ -34,6 +42,58 @@ const DataInput = () => {
           onChange={loadHistoryData}
         />
       </form> 
+      <Typography variant="h5" gutterBottom>
+        Your Top Channels
+      </Typography>
+      {topChannels.slice(0, 5).map((value,key) => {
+        return (
+          <Card>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Channel name">
+                  R
+                </Avatar>
+              }
+              title={value[0]}
+              subheader=""
+            />
+            <CardContent>
+              <Typography variant='subtitle2' gutterBottom>
+                Stats
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Number of videos watched: {value[1]}
+              </Typography>
+            </CardContent>
+          </Card>
+        )
+      })}
+      <Typography variant="h5" gutterBottom>
+        Your Top Videos
+      </Typography>
+      {topVideos.slice(0, 5).map((value,key) => {
+        return (
+          <Card>
+            <CardHeader
+              avatar={
+                <Avatar aria-label="Channel name">
+                  R
+                </Avatar>
+              }
+              title={value[0]}
+              subheader=""
+            />
+            <CardContent>
+              <Typography variant='subtitle2' gutterBottom>
+                Stats
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Number of times watched: {value[1]}
+              </Typography>
+            </CardContent>
+          </Card>
+        )
+      })}
     </>
   )
 }
